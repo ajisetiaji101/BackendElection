bc2065e18885dd03cb830bcd881026d4126efa08
Aji Setiaji <aji.setiaji@iconpln.co.id>
2025-01-31 15:18:26 +0700

    perbaikan data

--- migration/2.009_t_whitelistip.sql
+++ migration/2.009_t_whitelistip.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ip_whitelist (
     id SERIAL PRIMARY KEY,          -- Auto-incrementing unique identifier
-    ip_address INET NOT NULL,       -- IP address (supports both IPv4 and IPv6)
+    ip_address VARCHAR(255) NOT NULL,       -- IP address (supports both IPv4 and IPv6)
     description TEXT,               -- Optional description or reason for whitelisting
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the entry was created
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP -- Timestamp for the last update
